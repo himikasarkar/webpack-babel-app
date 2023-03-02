bb376310c417e031827c8536e8a2abd16abe773a
Himika Sarkar <himikas.work@gmail.com>
2023-03-03 03:41:00 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <h1>This is an Article on Webpack and Babel</h1>
+        <p>Author: Himika Sarkar</p>
+    </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('root'));
